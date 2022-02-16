e6b8bef7ed201e5659cc6480c45860a461681a78
MRaihanAlifA <muhammadraihanalif@gmail.com>
2022-02-16 11:51:32 +0700

    adding some code

--- data_list.py
+++ data_list.py
@@ -22,3 +22,25 @@ book_list.append('sociology')
 print('\nshow new book list')
 for i in range(0, len(book_list)):
     print(book_list[i])
+
+print('\nclear list')
+book_list.clear()
+print(book_list)
+
+book_list = ['mathematics', 'science', 'chemics', 'english']
+print('\nchange first element')
+book_list[0] = 'physics'
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\npick second element')
+new_book = book_list.pop(1)
+for i in range(0, len(book_list)):
+    print(book_list[i])
+print(f'second element = {new_book}')
+
+print('\npop -1')
+book_list = ['mathematics', 'science', 'chemics', 'english']
+book_list.pop(-1)
+for i in range(0, len(book_list)):
+    print(book_list[i])
